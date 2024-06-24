81f11de1e7fbc0248087e47105920f6b3f4becf4
abdullah <aba252004@gmail.com>
2024-06-24 21:33:55 +0300

    add new camera live

--- main.js
+++ main.js
@@ -14,19 +14,26 @@ const showMenu = (toggleId, navId) =>{
  
  showMenu('nav-toggle','nav-menu')
 
-//  document.addEventListener("DOMContentLoaded", function() {
-//     const navLinks = document.querySelector(".nav__link");
-  
-//     navLinks.forEach(function(link) {
-//       link.addEventListener("click", function() {
-//         // Remove 'active' class from all nav links
-//         navLinks.forEach(function(link) {
-//           link.classList.remove("active");
-//         });
-  
-//         // Add 'active' class to the parent of the clicked nav link (assuming the parent is the div)
-//         this.classList.add("active");
-//       });
-//     });
-//   });
-  
+ function navigateToNewPage() {
+    window.location.href = 'new-camera.html'; // Replace 'new-camera.html' with the path to your HTML file
+}
+//live cam online
+function addVideo() {
+    const url = document.getElementById('url').value;
+    let embedUrl = url;
+
+    if (url.includes('youtube.com/watch?v=')) {
+        const videoId = url.split('v=')[1].split('&')[0];
+        embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    } else if (url.includes('youtu.be/')) {
+        const videoId = url.split('youtu.be/')[1];
+        embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    }
+
+    if (embedUrl) {
+        const iframeContainer = document.getElementById('iframeContainer');
+        iframeContainer.innerHTML = `<iframe src="${embedUrl}" allowfullscreen></iframe>`;
+    } else {
+        alert('Please enter a valid URL.');
+    }
+}
