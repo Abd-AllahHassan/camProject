8b1a4e17f8264b02bb1e06aff1cbde370a357e28
abdullah <aba252004@gmail.com>
2024-06-30 01:32:53 +0300

    add new camera live

--- main.js
+++ main.js
@@ -20,7 +20,31 @@ const showMenu = (toggleId, navId) =>{
 //live cam online
 function addVideo() {
     const url = document.getElementById('url').value;
-   
+    let embedUrl = url;
+
+    if (url.includes('youtube.com/watch?v=')) {
+        const videoId = url.split('v=')[1].split('&')[0];
+        embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    } else if (url.includes('youtu.be/')) {
+        const videoId = url.split('youtu.be/')[1];
+        embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    }
+
+    if (embedUrl) {
         const iframeContainer = document.getElementById('iframeContainer');
-   
+        iframeContainer.innerHTML = `<iframe src="${embedUrl}" allowfullscreen></iframe>`;
+    } else {
+        alert('Please enter a valid URL.');
+    }
 }
+
+// Function to add HTML code directly
+function addVideoByHtml() {
+    const htmlCode = document.getElementById('HtmlCode').value;
+    const iframeContainer = document.getElementById('iframeContainer');
+    iframeContainer.innerHTML = htmlCode;
+}
+
+// Event listeners for buttons
+document.getElementById('addVideoButton').addEventListener('click', addVideo);
+document.getElementById('addHtmlButton').addEventListener('click', addVideoByHtml);
